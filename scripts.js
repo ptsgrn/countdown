751c3c1a85cbf884c4dbd743979f6a43cc8a07e7
ptsgrn <49602385+ptsgrn@users.noreply.github.com>
2020-01-17 14:47:09 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("20200608100623Z").getTime();
+var countDownDate = new Date(2020, 6, 8, 10, 6, 0, 0).getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
